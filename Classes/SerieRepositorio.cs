25a8950756eacdef76165083eaca51e062e00362
Genilton <geniltonbarbosadasilva@gmail.com>
2021-09-15 00:10:31 -0300

    SerieRepositorio

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        public List<Serie> Lista()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
